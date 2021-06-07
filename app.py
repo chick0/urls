221f8feb21699e50c22a3d24524eae861c1a3f82
chick_0 <chick_0@kakao.com>
2021-06-08 01:46:51 +0900

    update + superuser + memory cache + using argument parser

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
+from random import choice
+from base64 import b64decode
+from asyncio import sleep
 from secrets import token_bytes
 from sqlite3 import IntegrityError
+from argparse import ArgumentParser
 
 from sanic import Sanic
-from sanic.exceptions import SanicException
+from sanic.exceptions import SanicException, Unauthorized
 from sanic.response import html, redirect
 from aiosqlite import connect
 from jinja2 import Environment, PackageLoader, select_autoescape
@@ -13,6 +17,9 @@ env = Environment(
     loader=PackageLoader("urls", "templates", "utf-8"),
     autoescape=select_autoescape()
 )
+
+# Short URL Cache
+cache = {}
 
 
 async def get_db():
@@ -32,9 +39,7 @@ async def index(request):
 async def url_create(request):
     url = request.form.get("url", None)
     if url is None:
-        return redirect(
-            to=app.url_for("index")
-        )
+        return redirect(to="/")
 
     db = await get_db()
     cur = await db.cursor()
@@ -76,12 +81,13 @@ async def url_manage(request, code: str, magic: str):
     )
     ctx = await c.fetchone()
     if ctx is None:
-        return redirect(
-            to=app.url_for("index")
-        )
+        return redirect(to="/")
 
     if request.method == "GET":
         if request.args.get("delete", "no") == "yes":
+            if code in cache.keys():
+                del cache[code]
+
             await cur.execute(
                 "DELETE FROM urls WHERE code=? AND magic=?",
                 (code, magic)
@@ -111,40 +117,114 @@ async def url_manage(request, code: str, magic: str):
                 (new_url, code, magic)
             )
             await db.commit()
+            cache[code] = new_url
 
         await db.close()
         return redirect(
-            to=app.url_for("url_manage", code=code, magic=magic),
+            to=app.url_for("url_manage", code=code, magic=magic)
         )
+
+
+@app.route("/superuser")
+async def superuser(request):
+    if len(args.username) == 0 or len(args.password) == 0:
+        return redirect(to="/")
+
+    auth = request.headers.get("Authorization", None)
+
+    if auth is None:
+        raise Unauthorized("Auth required", scheme="Basic", realm="Auth required")
+    else:
+        type_, value_ = auth.split()
+        if type_.lower() == "basic":
+            username, password = b64decode(value_).decode().split(":")
+            if username == args.username and password == args.password:
+                db = await get_db()
+                cur = await db.cursor()
+
+                c = await cur.execute(
+                    "SELECT * FROM urls"
+                )
+
+                all_url = [
+                    {
+                        "code": ctx[0],
+                        "url": ctx[1],
+                        "magic": app.url_for("url_manage", code=ctx[0], magic=ctx[2])
+                    } for ctx in await c.fetchall()
+                ]
+
+                template = env.get_template("superuser.html")
+                return html(
+                    body=template.render(all_url=all_url)
+                )
+
+    return redirect(to="/")
 
 
 @app.route("/<code:string>")
 async def warp(request, code: str):
-    db = await get_db()
-    cur = await db.cursor()
+    if code not in cache.keys():
+        db = await get_db()
+        cur = await db.cursor()
 
-    c = await cur.execute(
-        "SELECT url FROM urls WHERE code=?",
-        (code,)
-    )
-    ctx = await c.fetchone()
-    await db.close()
-
-    if ctx is None:
-        raise SanicException("URL Not Found", 404)
-
-    url = ctx[0]
-    if url.startswith("http://") or url.startswith("https://"):
-        return redirect(
-            to=url
+        c = await cur.execute(
+            "SELECT url FROM urls WHERE code=?",
+            (code,)
         )
+        ctx = await c.fetchone()
+        await db.close()
+
+        if ctx is None:
+            raise SanicException("URL Not Found", 404)
+
+        url = ctx[0]
+        cache[code] = url
+    else:
+        url = cache[code]
+
+    if url.startswith("http://") or url.startswith("https://"):
+        return redirect(to=url)
     else:
         raise SanicException("URL does not start with http or https!", 400)
 
 
+async def clean_up():
+    while args.limit < len(cache):
+        del cache[choice(list(cache.keys()))]
+
+    await sleep(60)
+    await clean_up()
+
+
 if __name__ == "__main__":
+    parser = ArgumentParser(
+        description="simple URL Shorter"
+    )
+
+    parser.add_argument("--host",
+                        help="set host ip address",
+                        action="store", type=str, default="0.0.0.0")
+    parser.add_argument("--port",
+                        help="set port number",
+                        action="store", type=int, default=8000)
+
+    parser.add_argument("--limit",
+                        help="set cache limit for url cache",
+                        action="store", type=int, default=2500)
+
+    parser.add_argument("--username",
+                        help="set superuser username",
+                        action="store", type=str, default="")
+    parser.add_argument("--password",
+                        help="set superuser password",
+                        action="store", type=str, default="")
+
+    args = parser.parse_args()
+
+    app.add_task(clean_up)
     app.run(
-        host="0.0.0.0",
-        port=8000,
+        host=args.host,
+        port=args.port,
         debug=False
     )
